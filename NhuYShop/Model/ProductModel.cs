b9f649104993ca25c0c046b4f093ebdd49f28308
Trần Hoàng <wujor01@gmail.com>
2021-02-13 07:35:47 +0700

    add product backend

---
+++ NhuYShop/Model/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NhuYShop.Model
+{
+    public class ProductModel
+    {
+        public int PRODUCTID { get; set; }
+        public string PRODUCTNAME { get; set; }
+        public int VALUE1 { get; set; }
+        public int VALUE2 { get; set; }
+        public int VALUE3 { get; set; }
+    }
+}
